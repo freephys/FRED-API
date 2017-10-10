eb5088d64c215b6acf8d9308851ced96d01dddb7
John Tsang <wk3tsang@uwaterloo.ca>
2017-10-10 07:14:54 -0400

    Delete example2.py

--- example2.py
+++
@@ -1,8 +0,0 @@
-r = get_obs_json("API_KEY","GNPCA",observation_end = "2013-01-01")
-import sqlite3
-conn = sqlite3.connect('time_series3.sqlite')
-cur = conn.cursor()
-r.df.to_sql("GNPCA",conn)
-conn.commit()
-conn.close()
-fetch_FRED_data(db_file_name = 'time_series3.sqlite', series_id="GNPCA", api_key="API_KEY")
